914ed6a3010cc07b8370d4fbc98030eadfe19a38
Steven B <steven@botzek.com>
2023-02-10 21:45:31 -0800

    Use bulma css

--- src/dashcrawl/webclient.cljs
+++ src/dashcrawl/webclient.cljs
@@ -25,15 +25,26 @@
   (let [message-field (r/atom nil)
         message (rf/subscribe [:message])]
     (fn []
-      [:div
-       [:h1 "Set the Message"]
+      [:div.container
+       [:h1.title "Set the Message"]
        [:div @message]
        [:br]
        [:form {:on-submit (fn update-message [e]
                             (.preventDefault e)
-                            (rf/dispatch [:set-message @message-field]))}
-        [:input {:type :text :on-change (fn update-field[e] (reset! message-field (.. e -target -value)))}]
-        [:input {:type :submit :value "Set Message"}]]])))
+                            (rf/dispatch [:set-message @message-field])
+                            (reset! message-field "")
+                            (-> js/document
+                                (.getElementById "message-input")
+                                (.-value)
+                                (set! "")))}
+        [:div.field
+         [:label.label "Name"]
+         [:div.control
+          [:input#message-input.input {:type :text
+                                       :placeholder "Message"
+                                       :on-change (fn update-field[e]
+                                                    (reset! message-field (.. e -target -value)))}]]]
+        [:input.button {:type :submit :value "Set Message"}]]])))
 
 (defn mount-root
   []
